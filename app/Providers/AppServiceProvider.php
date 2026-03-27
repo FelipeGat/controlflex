320ec4e1bf75b254bf118fc5cd637eddae5fc226
rossini06 <rossinidesouza06@gmail.com>
2026-03-27 16:28:52 +0000

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // Garante que todos os redirects e URLs usem a APP_URL correta (incluindo porta)
+        URL::forceScheme('https');
         URL::forceRootUrl(config('app.url'));
     }
 }
